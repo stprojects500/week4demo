6677c182a01c700e0845b1bf7278b4c4f69c4cb3
stprojects500 <stanimira500@abv.bg>
2019-10-25 13:53:25 +0300

    Innitial commit

---
+++ Week4Workshop/Registration.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Registration.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
