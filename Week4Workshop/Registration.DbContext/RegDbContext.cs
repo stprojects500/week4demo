e1e3142dfa2227f302a21159f94c179d97523018
stprojects500 <stanimira500@abv.bg>
2019-10-25 14:34:33 +0300

    innitial commit

---
+++ Week4Workshop/Registration.DbContext/RegDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Registration.DBContext
+{
+    class RegDbContext : DbContext
+    {
+    }
+}
